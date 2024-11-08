4d5ea86ac7e26a54c181e9ddb79eab4d506657f9
“pedro23071 <Pedro23071@gmail.com>
2024-11-07 22:29:22 -0600

    templates

--- 11_django/mi_proyecto/mi_proyecto/urls.py
+++ 11_django/mi_proyecto/mi_proyecto/urls.py
@@ -15,7 +15,12 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from myapp import views as myapp_views
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', myapp_views.index, name="index"),
+    path('pagina/', myapp_views.pagina, name="pagina"),
+    path('hola-mundo', myapp_views.hola_mundo, name="hola_mundo")
 ]
--- 11_django/mi_proyecto/myapp/views.py
+++ 11_django/mi_proyecto/myapp/views.py
@@ -1,3 +1,14 @@
-from django.shortcuts import render
+from django.shortcuts import render,  HttpResponse
 
 # Create your views here.
+
+def index(request):
+    return HttpResponse("hola desde INICIO")
+
+def pagina(request):
+    return HttpResponse("hola desde Pagina")
+
+def hola_mundo(request):
+    return HttpResponse("Hola mundo desde mi controlador")
+
+
