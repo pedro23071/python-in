f5fcdddbdb80d01a80e7fe38bd217544e36cd75a
Pedro A <juan-delprado@hotmail.com>
2024-12-03 11:23:41 -0600

    agregar orden y si es visible en navbar

--- 12_proyecto/ProjectoDjngo/pages/context_processors.py
+++ 12_proyecto/ProjectoDjngo/pages/context_processors.py
@@ -2,7 +2,7 @@ from .models import Page
 
 
 def get_pages(request):
-    pages = Page.objects.filter(visible=True).values_list('id', 'title', 'slug')
+    pages = Page.objects.filter(visible=True, is_nav=True).values_list('id', 'title', 'slug').order_by('order')
     return {
         'pages': pages
     }
--- 12_proyecto/ProjectoDjngo/pages/models.py
+++ 12_proyecto/ProjectoDjngo/pages/models.py
@@ -6,6 +6,8 @@ class Page(models.Model):
     title = models.CharField(max_length=50, verbose_name='Titulo')
     content = RichTextField(verbose_name='Contenido')
     slug =  models.CharField(unique=True, max_length=150, verbose_name='URL Amigable')
+    order = models.IntegerField(default=0, verbose_name='Orden')
+    is_nav = models.BooleanField(default=False, verbose_name='En Navegacion')
     visible = models.BooleanField(verbose_name='Visible')
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
