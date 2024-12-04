23467747ec9bf6c8d4ab9938952c85bfd286ef90
Pedro A <juan-delprado@hotmail.com>
2024-12-04 16:46:53 -0600

    url para app blog

--- 12_proyecto/ProjectoDjngo/ProjectoDjngo/urls.py
+++ 12_proyecto/ProjectoDjngo/ProjectoDjngo/urls.py
@@ -21,4 +21,5 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('mainapp.urls')),
     path('', include('pages.urls')),
+    path('', include('blog.urls')),
 ]
--- 12_proyecto/ProjectoDjngo/blog/views.py
+++ 12_proyecto/ProjectoDjngo/blog/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
+from .models import Article
 
 # Create your views here.
+def list_articles(request):
+    articles = Article.objects.all()
+    return render(request, 'article/list.html', {
+        'title': 'Listado de Articulos',
+        'articles_list': articles,
+     })
